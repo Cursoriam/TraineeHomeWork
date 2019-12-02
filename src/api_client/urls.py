4c088d1f9d13f9ecfd76e012764c536ae81caba9
Vladimir Tokarev <coaka2013@gmail.com>
2019-12-02 04:27:56 +0300

    add response serializer to login_serializers.py

--- src/api_client/urls.py
+++ src/api_client/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from api_client import views
 
 urlpatterns: list = [
-    path('client', views.ClientView.as_view()),
-    path('client/<str:id>', views.ClientViewToDelete.as_view()),
+    path('register', views.ClientView.as_view()),
+    path('client/<str:login>', views.ClientViewToDelete.as_view()),
     path('authenticate', views.UserAuthenticate.as_view()),
     path('authorize', views.UserAutorization.as_view()),
-    path('transcript', views.SpeechTranscriptView.as_view()),
+    path('<str:id>/followings', views.GetFollowersView.as_view()),
+    path('<str:follower_id>/follow/<str:user_id>',
+         views.FollowerView.as_view()),
+    path('<str:id>/make_pitt', views.FollowerView.as_view()),
+    path('<str:login>/pitts/<pitt:id>', views.DeletePittView.as_view()),
+    path('<str:login>/feed/page=<int: page>', views.FeedView.as_view()),
 ]
