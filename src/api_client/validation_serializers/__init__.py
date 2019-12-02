037786244c9326be64a975b62f434c14f7bb327c
Vladimir Tokarev <coaka2013@gmail.com>
2019-12-02 04:29:11 +0300

    fix init file of serializers

--- src/api_client/validation_serializers/__init__.py
+++ src/api_client/validation_serializers/__init__.py
@@ -3,11 +3,23 @@ from typing import List, Callable
 from .client_serializers import ClientPostRequest
 from .client_serializers import ClientPostResponse
 from .login_serializers import ClientLoginRequest
+from .login_serializers import ClientLoginResponse
 from .transcript_serializers import TranscriptPostRequest
+from .following_serializers import FollowerPostRequest
+from .following_serializers import FollowerPostResponse
+from .following_serializers import FollowerGetResponse
+from .pitt_response import PittPostResponse
+from .feed_serializers import FeedGetResponse
 
 __all__: List[Callable] = [
     'ClientPostRequest',
     'ClientPostResponse',
     'ClientLoginRequest',
+    'ClientLoginResponse',
     'TranscriptPostRequest',
+    'FollowerPostRequest',
+    'FollowerPostResponse',
+    'PittPostResponse',
+    'FeedGetResponse',
+    'FollowerGetResponse',
 ]
