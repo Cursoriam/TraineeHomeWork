77d61d824bd9cdce842995a80a9a304a3b9a9179
Vladimir Tokarev <coaka2013@gmail.com>
2019-12-09 15:21:26 +0300

    fix request in speech_transcription

--- src/pitter/utils/speech_transcript.py
+++ src/pitter/utils/speech_transcript.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from .validation import check_filepath
 from pitter import settings
@@ -7,11 +8,17 @@ from pitter import exceptions
 
 def speech_transcript(filepath):
     check_filepath(filepath)
+    filepath = {'filepath': filepath}
     try:
         text = requests.post(settings.SPEECH_TRANSCRIPT_URL,
                              data=filepath)
     except (ValueError, RuntimeError, TypeError, NameError,
             requests.exceptions.RequestException):
         raise exceptions.STTResponseError()
+
+    if text.status_code == 500:
+        raise exceptions.STTResponseError()
+    text = json.loads(text.text)
+    text = text['text']
 
     return text
